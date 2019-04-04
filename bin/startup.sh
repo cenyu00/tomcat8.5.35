dd6949fc9de145c0dfd065bb2aefcc16304b5b96
QiPeng <qipeng.lu@bintools.com>
2019-04-04 10:22:55 +0800

    startup.sh脚本

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,22 +1,8 @@
 #!/bin/sh
 
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # -----------------------------------------------------------------------------
-# Start Script for the CATALINA Server
+# Catalina服务的启动脚本
+# 该脚本的主要功能是检查catalina.sh的脚本是否为链接，然后启动这个脚本
 # -----------------------------------------------------------------------------
 
 # Better OS/400 detection: see Bugzilla 31132
@@ -26,6 +12,7 @@ OS400*) os400=true;;
 esac
 
 # resolve links - $0 may be a softlink
+# $0表示shell脚本的名称
 PRG="$0"
 
 while [ -h "$PRG" ] ; do
